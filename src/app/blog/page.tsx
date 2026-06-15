7c055636c2b271fc0a40198eeeb022b4b67eaf2f
manhhuy140805 <trandinhmanhhuy05@gmail.com>
2026-06-15 09:00:07 +0700

    feat: page/blog

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,10 @@
+import BlogComingSoon from "@/src/components/blog/BlogComingSoon";
+
+export const metadata = {
+  title: "Blog | Portfolio",
+  description: "Trang blog cá nhân đang được xây dựng.",
+};
+
+export default function BlogPage() {
+  return <BlogComingSoon />;
+}
